596e23383b33890b8d4e91f4099b8cfd01396634
Nicolas Schejtman <nschejtman93@gmail.com>
2021-08-17 11:53:09 -0300

    APIMF-3265: add profile validation test

---
+++ scripts/validate-profiles.sh
@@ -0,0 +1,16 @@
+rm validation-profile.yaml
+curl https://raw.githubusercontent.com/aml-org/models/master/src/main/dialects/validation-profile.yaml >> validation-profile.yaml
+profiles=$(grep -rw './' -e "#%Validation Profile" | cut -b 4- | cut -d ":" -f1 | grep .yaml)
+for profile in $profiles
+do
+  report=$(java -jar amf.jar validate -in "AML 1.0" -mime-in "application/yaml" -ds file://validation-profile.yaml file://$profile)
+  if grep -q '"http://www.w3.org/ns/shacl#conforms": true' <<< "$report";
+  then
+    echo "\033[32m $profile conforms \033[0m"
+  else
+    echo "\033[31m $profile does not conform \033[0m"
+#    echo $report
+  fi
+done
+
+
