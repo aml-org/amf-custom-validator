4d26abe5bfdd649e19ebf1385c5c2c12cf7505d0
Nicolas Schejtman <nschejtman93@gmail.com>
2021-08-17 16:09:16 -0300

    Zip WASM to reduce JS artifact size

--- scripts/gen_js_package.sh
+++ scripts/gen_js_package.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 GOOS=js GOARCH=wasm go build -o wrappers/js/lib/main.wasm -ldflags "-s -w" js/validator.go
+gzip -9 -v -c wrappers/js/lib/main.wasm > wrappers/js/lib/main.wasm.gz
+rm wrappers/js/lib/main.wasm
