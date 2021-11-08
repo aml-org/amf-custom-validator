75bbf6cf4e90c1a10eef770f21c59ab7f7ae6d03
Nicolas Schejtman <nschejtman93@gmail.com>
2021-11-08 15:40:40 -0300

    Modify download dir for dialects & vocabularies

--- scripts/validate-profiles.sh
+++ scripts/validate-profiles.sh
@@ -1,9 +1,10 @@
 rm validation-profile.yaml
-curl https://raw.githubusercontent.com/aml-org/models/master/src/main/dialects/validation-profile.yaml >> validation-profile.yaml
+mkdir dialects vocabularies
+curl https://raw.githubusercontent.com/aml-org/models/master/src/main/dialects/validation-profile.yaml >> dialects/validation-profile.yaml
 profiles=$(grep -rw './' -e "#%Validation Profile" | cut -b 4- | cut -d ":" -f1 | grep .yaml)
 for profile in $profiles
 do
-  report=$(java -jar amf.jar validate -in "AML 1.0" -mime-in "application/yaml" -ds file://validation-profile.yaml file://$profile)
+  report=$(java -jar amf.jar validate -in "AML 1.0" -mime-in "application/yaml" -ds file://dialects/validation-profile.yaml file://$profile)
   if grep -q '"http://www.w3.org/ns/shacl#conforms": true' <<< "$report";
   then
     echo "\033[32m $profile conforms \033[0m"
