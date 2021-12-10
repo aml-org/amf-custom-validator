3b1604ebdceaafce5b0d85178eb34b53f57af268
Tomas Fernandez <tfernandez@mulesoft.com>
2021-12-10 11:15:05 -0300

    Fix: changed null for undefineds as return type of results or errors

--- wrappers/js-web/index.js
+++ wrappers/js-web/index.js
@@ -4,7 +4,7 @@ const pako = require("pako");
 const Buffer = require("buffer").Buffer;
 
 let initialized = false
-let go = null;
+let go = undefined;
 let wasm;
 
 const run = function(profile, data, debug) {
@@ -18,15 +18,15 @@ const run = function(profile, data, debug) {
 const validateCustomProfile = function(profile, data, debug, cb) {
     if (initialized) {
         let res = run(profile, data, debug);
-        cb(res,null);
+        cb(res, undefined);
     } else {
-        cb(null,new Error("WASM/GO not initialized"))
+        cb(undefined, new Error("WASM/GO not initialized"))
     }
 }
 
 const initialize = function(cb) {
     if (initialized === true) {
-        cb(null);
+        cb(undefined);
     }
     go = new Go();
     if(!wasm_gz || !wasm) {
@@ -36,7 +36,7 @@ const initialize = function(cb) {
         WebAssembly.instantiate(wasm, go.importObject).then((result) => {
             go.run(result.instance);
             initialized = true;
-            cb(null);
+            cb(undefined);
         });
     } else {
         cb(new Error("WebAssembly is not supported in your JS environment"));
