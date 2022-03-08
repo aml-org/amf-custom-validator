1791dfa843e513b3fd129d472e718c3930295a36
Agustin Alexander <agustin.alexander@mulesoft.com>
2022-03-08 17:29:42 -0300

    Blocked http send built in function

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/open-policy-agent/opa v0.28.0
 	github.com/piprate/json-gold v0.4.0
+	github.com/stretchr/testify v1.4.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
--- internal/validator/validate_integration_test.go
+++ internal/validator/validate_integration_test.go
@@ -4,9 +4,34 @@ import (
 	"github.com/aml-org/amf-custom-validator/internal/config"
 	"github.com/aml-org/amf-custom-validator/internal/parser/profile"
 	"github.com/aml-org/amf-custom-validator/test"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"regexp"
 	"strings"
 	"testing"
 )
+
+func TestBuiltInHttpDisabled(t *testing.T) {
+	prof, err := os.ReadFile("../../test/data/integration/built-in-security/http-block.yaml")
+	assert.Empty(t, err)
+	assert.NotEqual(t, len(prof), 0)
+	jsonldText, err := os.ReadFile("../../test/data/integration/built-in-security/positive.data.jsonld")
+	assert.NotEqual(t, len(jsonldText), 0)
+	assert.Empty(t, err)
+	_, err = Validate(string(prof), string(jsonldText), false, nil)
+	if err == nil {
+		assert.Fail(t, "Expected built-in security error")
+	} else {
+
+		re := regexp.MustCompile(" profile_testblock.rego:[0-9]+: rego_type_error: ")
+
+		split := re.Split(err.Error(), -1)
+
+		expected := []string{"1 error occurred:", "unsafe built-in function calls in expression: http.send"}
+
+		assert.Equal(t, expected, split, "Expected validation to fail due to security violations")
+	}
+}
 
 func TestIntegrationPositiveData(t *testing.T) {
 	filter := ""
