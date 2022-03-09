100ca0bb3a465aa870330cca5c92fa51cd3ddb55
Nicolas Schejtman <nschejtman93@gmail.com>
2022-03-09 20:09:35 -0300

    W-10815386: block unsafe builtin Rego functions

--- internal/validator/process_profile.go
+++ internal/validator/process_profile.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aml-org/amf-custom-validator/internal/generator"
 	"github.com/aml-org/amf-custom-validator/internal/parser"
 	e "github.com/aml-org/amf-custom-validator/pkg/events"
+	"github.com/open-policy-agent/opa/ast"
 	"github.com/open-policy-agent/opa/rego"
 )
 
@@ -50,11 +51,20 @@ func GenerateRego(profileText string, debug bool, eventChan *chan e.Event) (*gen
 	return &module, err
 }
 
+// unsafeBuiltinsMap When updating to 0.35 ast.NetLookupIPAddr will be available and needs to be added and blocked too
+var unsafeBuiltinsMap = map[string]struct{}{
+	ast.HTTPSend.Name:        {},
+	ast.WalkBuiltin.Name:     {},
+	ast.OPARuntime.Name:      {},
+	ast.RegoParseModule.Name: {},
+}
+
 func CompileRego(regoUnit *generator.RegoUnit, eventChan *chan e.Event) (*rego.PreparedEvalQuery, error) {
 	dispatchEvent(e.NewEvent(e.RegoCompilationStart), eventChan)
 	query := rego.Query("data." + regoUnit.Name + "." + regoUnit.Entrypoint)
 	module := rego.Module(regoUnit.Name+".rego", regoUnit.Code)
-	preparedEvalQuery, err := rego.New(query, module).PrepareForEval(context.Background())
+	unsafeBuiltins := rego.UnsafeBuiltins(unsafeBuiltinsMap)
+	preparedEvalQuery, err := rego.New(query, module, unsafeBuiltins).PrepareForEval(context.Background())
 	dispatchEvent(e.NewEvent(e.RegoCompilationDone), eventChan)
 	return &preparedEvalQuery, err
 }
