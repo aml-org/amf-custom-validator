0517fa6e3a29f9faa445c64ebf7fbd19c7f8a9b0
nschejtman <nschejtman93@gmail.com>
2023-11-06 11:50:27 -0300

    Retrieving string from Go in JS

--- js/validator.go
+++ js/validator.go
@@ -2,21 +2,32 @@ package main
 
 // #include <stdlib.h>
 import "C"
-import "unsafe"
+import (
+	"unsafe"
+)
 
 // main is required for TinyGo to compile to Wasm.
 func main() {}
 
-//go:wasmimport env log
-func _log(ptr, size uint32)
+//go:wasmimport env assignResult
+func _assignResult(ptr, size uint32)
 
 func stringToPtr(s string) (uint32, uint32) {
 	ptr := unsafe.Pointer(unsafe.StringData(s))
 	return uint32(uintptr(ptr)), uint32(len(s))
 }
 
+func stringToLeakedPtr(s string) (uint32, uint32) {
+	size := C.ulong(len(s))
+	ptr := unsafe.Pointer(C.malloc(size))
+	copy(unsafe.Slice((*byte)(ptr), size), s)
+	return uint32(uintptr(ptr)), uint32(size)
+}
+
 //export greet
-func greet() {
-	ptr, size := stringToPtr("ASD FROM GO")
-	_log(ptr, size)
+func greet(name string) {
+	message := "ASD FROM GO"
+	ptr, size := stringToPtr(message)
+	//runtime.KeepAlive(message)
+	_assignResult(ptr, size)
 }
--- wrappers/js/index.js
+++ wrappers/js/index.js
@@ -12,16 +12,14 @@ let wasmModule, wasmInst
 async function run() {
     const source = fs.readFileSync("./lib/main.wasm")
 
-    function log(ptr, size) {
-        console.log(memToString(ptr, size))
-    }
-
-    function memToString(ptr, len) {
+    let result
+    function assignResult(ptr, len) {
+        console.log(`Assigning result: {ptr: ${ptr}, len: ${len}}`)
         const buf = new Uint8Array(wasmInst.exports.memory.buffer, ptr, len)
-        return new TextDecoder('utf8').decode(buf)
+        result = new TextDecoder('utf8').decode(buf);
     }
 
-    const env = {log}
+    const env = {assignResult}
     wasmModule = await WebAssembly.compile(source)
     go.importObject.env = Object.assign(go.importObject.env, env)
     wasmInst = await WebAssembly.instantiate(wasmModule, go.importObject)
@@ -29,7 +27,9 @@ async function run() {
 
 
 
-    wasmInst.exports.greet()
+    wasmInst.exports.greet("nico")
+    console.log("Result")
+    console.log(result)
 }
 
 
