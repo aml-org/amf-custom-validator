d90a8f66473ea0e4bb57d78b0f238648550f2329
nschejtman <nschejtman93@gmail.com>
2023-12-06 13:41:27 -0300

    Minor changes

--- scripts/gen_js_package.sh
+++ scripts/gen_js_package.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#GOOS=js GOARCH=wasm go build -o wrappers/js/lib/main.wasm js/validator.go # use this to debug
 GOOS=js GOARCH=wasm go build -o wrappers/js/lib/main.wasm -ldflags "-s -w" js/validator.go
 gzip -9 -v -c wrappers/js/lib/main.wasm > wrappers/js/lib/main.wasm.gz
 rm wrappers/js/lib/main.wasm
