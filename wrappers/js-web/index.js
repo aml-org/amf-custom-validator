da5caea13d25391a60b5f7eef9b2cc7d60366e55
nschejtman <nschejtman93@gmail.com>
2024-01-03 11:43:46 -0300

    Browser > refactor index.js + added functions from node in browser

--- wrappers/js-web/index.js
+++ wrappers/js-web/index.js
@@ -1,30 +1,24 @@
+
+// Go glue code
 require('./lib/wasm_exec');
+
+// WASM
 const wasm_gz = require('../js/lib/main.wasm.gz')
+
+// Deps
 const pako = require('pako');
 
+// Vars
 let initialized = false
 let go = undefined;
 let wasm;
 
-const validateKernel = function(profile, data, debug) {
-    let before = new Date()
-    const res = __AMF__validateCustomProfile(profile,data, debug);
-    let after = new Date();
-    if (debug) console.log('Elapsed : ' + (after - before))
-    return res;
-}
-
-const validateWithReportConfigurationKernel = function(profile, data, debug, reportConfig) {
-    let before = new Date()
-    const res = __AMF__validateCustomProfileWithConfiguration(profile,data, debug, undefined, reportConfig);
-    let after = new Date();
-    if (debug) console.log('Elapsed : ' + (after - before))
-    return res;
-}
-
 const validateCustomProfile = function(profile, data, debug, cb) {
     if (initialized) {
-        let res = validateKernel(profile, data, debug);
+        let before = new Date()
+        const res = __AMF__validateCustomProfile(profile,data, debug);
+        let after = new Date();
+        if (debug) console.log('Elapsed : ' + (after - before))
         cb(res, undefined);
     } else {
         cb(undefined, new Error('WASM/GO not initialized'))
@@ -33,7 +27,10 @@ const validateCustomProfile = function(profile, data, debug, cb) {
 
 const validateCustomProfileWithReportConfiguration = function(profile, data, debug, reportConfig, cb) {
     if (initialized) {
-        let res = validateWithReportConfigurationKernel(profile, data, debug, reportConfig);
+        let before = new Date()
+        const res = __AMF__validateCustomProfileWithConfiguration(profile,data, debug, undefined, reportConfig);
+        let after = new Date();
+        if (debug) console.log('Elapsed : ' + (after - before))
         cb(res,undefined);
     } else {
         cb(undefined,new Error('WASM/GO not initialized'))
@@ -57,7 +54,24 @@ const initialize = function(cb) {
     } else {
         cb(new Error('WebAssembly is not supported in your JS environment'));
     }
+}
 
+const generateRego = function(profile, cb) {
+    if (initialized) {
+        const res = __AMF__generateRego(profile);
+        cb(res,undefined);
+    } else {
+        cb(undefined,new Error("WASM/GO not initialized"))
+    }
+}
+
+const normalizeInput = function(data, cb) {
+    if (initialized) {
+        const res = __AMF__normalizeInput(data);
+        cb(res,undefined);
+    } else {
+        cb(undefined,new Error("WASM/GO not initialized"))
+    }
 }
 
 const exit = function() {
@@ -71,4 +85,6 @@ const exit = function() {
 module.exports.initialize = initialize;
 module.exports.validate = validateCustomProfile;
 module.exports.validateWithReportConfiguration = validateCustomProfileWithReportConfiguration;
+module.exports.generateRego = generateRego;
+module.exports.normalizeInput = normalizeInput;
 module.exports.exit = exit;
